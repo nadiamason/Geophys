9da2aecfc9202076da10e81b3cd8ad00990ef56a
Nadia Mason <nm1819@ic.ac.uk>
2021-11-09 16:08:55 +0000

    made it function for generic use

--- makingcoords.py
+++ makingcoords.py
@@ -6,23 +6,27 @@ Ideally aim is to finish this file with a list of
 [(175, -30), (185, -30), (185, -43), (175, -43)] format
 a list, with arrays of (long, lat) """
 
+# kmlfile name is area-polygons.kml
+# newtxtfile name is to be area-polygons.txt
+def kmltotxt(kmlfile, newtxtfile):
+    infile = open("%s" % (kmlfile))
+    newfile = open("%s" % (newtxtfile), "w")
+    counter = 0
+    for line in infile:
+        words = line.split()
+        firstword = words[0]
+        if firstword[0] == "<":
+            continue
+        counter += 1
+        polygon = []
 
-infile = open("C:/Users/nadia/Documents/University/geophys project/allpolygons.kml")
-newfile = open("polygoncoords.txt", "w")
-counter = 0
-for line in infile:
-    words = line.split()
-    firstword = words[0]
-    if firstword[0] == "<":
-        continue
-    counter += 1
-    polygon = []
+        for i in words:
+            list_numbers = i.split(",")
+            altitude = list_numbers.pop(2)
+            polygon.append(list_numbers)
+        
+        newfile.write(str(polygon))
+        newfile.write("\n")
 
-    for i in words:
-        list_numbers = i.split(",")
-        altitude = list_numbers.pop(2)
-        polygon.append(list_numbers)
-    
-    newfile.write(str(polygon))
-    newfile.write("\n")
+kmltotxt("northview-polygons.kml", "northview-polygons.txt")
         
