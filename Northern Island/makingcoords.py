df9a100829301029d7da792941362f58df635aad
Nadia Mason <nm1819@ic.ac.uk>
2021-11-13 15:14:34 +0000

    Northern view

---
+++ Northern Island/makingcoords.py
@@ -0,0 +1,32 @@
+import numpy as np
+# a program to convert kml files to longitude and latitude coords
+"""KML has cordinates [Long, Lat, Alt, ' ', Long, Lat, Alt, ' '] 
+we don't care about altitude, so we want to remove them and also the space
+Ideally aim is to finish this file with a list of 
+[(175, -30), (185, -30), (185, -43), (175, -43)] format
+a list, with arrays of (long, lat) """
+
+# kmlfile name is area-polygons.kml
+# newtxtfile name is to be area-polygons.txt
+def kmltotxt(kmlfile, newtxtfile):
+    infile = open("%s" % (kmlfile))
+    newfile = open("%s" % (newtxtfile), "w")
+    counter = 0
+    for line in infile:
+        words = line.split()
+        firstword = words[0]
+        if firstword[0] == "<":
+            continue
+        counter += 1
+        polygon = []
+
+        for i in words:
+            list_numbers = i.split(",")
+            altitude = list_numbers.pop(2)
+            polygon.append(list_numbers)
+        
+        newfile.write(str(polygon))
+        newfile.write("\n")
+
+kmltotxt("northernisland-polygons.kml", "northernisland-polygons.txt")
+        
